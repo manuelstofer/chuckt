bb0e84fd21d281ee6d3aa75fd4659acc6ab1a9ef
Manuel Stofer <manuelstofer@gmail.com>
2014-01-16 18:01:28 +0900

    Adds an example for using ChuckT over SockJS

---
+++ examples/sockjs-node/server.js
@@ -0,0 +1,24 @@
+var http = require('http');
+var sockjs = require('sockjs');
+var chucktjs = require('../../lib/chuckt');
+
+var echo = sockjs.createServer();
+echo.on('connection', function(conn) {
+
+  var chuckt = chucktjs.chucktifySockJSNode(conn);
+
+  chuckt.on('echo', function() {
+    console.log('echo');
+    var args = Array.prototype.slice.call(arguments, 0);
+    var callback = args.pop();
+    callback.apply(null, args);
+  });
+
+  chuckt.emit('hello', 'hello client', function (response) {
+    console.log(response);
+  });
+});
+
+var server = http.createServer();
+echo.installHandlers(server, {prefix:'/echo'});
+server.listen(9999, '0.0.0.0');
